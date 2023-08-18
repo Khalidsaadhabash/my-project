0a81b02c36c2be4a816bf4a5058fde0a217476ca
Khalidsaadhabash <a0500394433@gmail.com>
2023-08-18 19:23:26 +0300

    add fun

--- main.js
+++ main.js
@@ -2,4 +2,14 @@ let dynamicIMG=document.getElementById('dynamicIMG')
 let correct=document.getElementById('corr')
 let No=document.getElementById('uncorr')
 
-const dynamicIMG1=['khalid__5_-removebg-preview.png','Fahad__1_-removebg-preview.png','Bander__1_-removebg-preview.png','Mohamd1__1_-removebg-preview.png' ,'mohamd2__1_-removebg-preview.png']
+const dynamicIMG1=['khalid__5_-removebg-preview.png','Fahad__1_-removebg-preview.png','Bander__1_-removebg-preview.png','Mohamd1__1_-removebg-preview.png' ,'mohamd2__1_-removebg-preview.png',]
+
+
+function imgRandom(){
+    let takeRandom=Math.floor((Math.random() *dynamicIMG1.length))
+    dynamicIMG.style.backgroundImage = `url("./img/${dynamicIMG1[takeRandom]}")`
+    
+
+}
+
+let theTimer=setInterval(imgRandom,100);
