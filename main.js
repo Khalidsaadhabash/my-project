d223c7bea1d73c68ae068bdd3496f12fcfc68409
Khalidsaadhabash <a0500394433@gmail.com>
2023-08-18 19:15:10 +0300

    add img without background

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 let dynamicIMG=document.getElementById('dynamicIMG')
 let correct=document.getElementById('corr')
 let No=document.getElementById('uncorr')
+
+const dynamicIMG1=['khalid__5_-removebg-preview.png','Fahad__1_-removebg-preview.png','Bander__1_-removebg-preview.png','Mohamd1__1_-removebg-preview.png' ,'mohamd2__1_-removebg-preview.png']
