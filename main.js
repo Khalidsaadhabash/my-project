5dfd18c06a9fd1230ceb1c2847525c986b960147
Khalidsaadhabash <a0500394433@gmail.com>
2023-08-18 19:41:01 +0300

    dooone

--- main.js
+++ main.js
@@ -13,3 +13,40 @@ function imgRandom(){
 }
 
 let theTimer=setInterval(imgRandom,100);
+
+const marK=()=>{
+    setTimeout(function (){
+        theTimer=setInterval(imgRandom,100)
+        document.getElementById('btn').disabled = false
+        document.getElementById('btn').style.opacity ='100%'
+    },2000)
+
+}
+
+let correct1=0;
+let No1=0;
+
+const istaag =() =>{
+    if(dynamicIMG.style.backgroundImage === `url("./img/${dynamicIMG1[0]}")`){
+    correct1++;
+    correct.innerText =correct1;
+    clearInterval(theTimer)
+
+    document.getElementById('btn').disabled =true;
+    document.getElementById('btn').style.opacity ='50%';
+
+    marK();
+
+    }else{
+        No1++;
+        No.innerText=No1;
+        clearInterval(theTimer);
+
+        document.getElementById('btn').disabled =true;
+        document.getElementById('btn').style.opacity ='50%';
+
+    marK();
+
+
+    }
+}
